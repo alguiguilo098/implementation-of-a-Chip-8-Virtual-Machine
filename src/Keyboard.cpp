25bb5a4e0fb1cfca7548c5aecea4f21b220d7a7f
felipe gomes <felgom@alunos.utfpr.edu.br>
2025-10-24 01:03:35 -0300

    add keyboard 🖥️
    adicionei todas as teclas, ficou parecendo gpt mas fiz namoral, eu so peguei as coisas do foleis na aula

---
+++ src/Keyboard.cpp
@@ -0,0 +1,23 @@
+#include "Keyboard.hpp"
+
+Keyboard::Keyboard() {
+    keyStates.fill(false);
+}
+
+void Keyboard::Update() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+            quit = true;
+        } else if (e.type == SDL_KEYDOWN) {
+            keyStates[e.key.keysym.scancode] = true;
+        } else if (e.type == SDL_KEYUP) {
+            keyStates[e.key.keysym.scancode] = false;
+        }
+    }
+}
+
+bool Keyboard::IsKeyPressed(SDL_Keycode key) const {
+    SDL_Scancode scancode = SDL_GetScancodeFromKey(key);
+    return keyStates[scancode];
+}
